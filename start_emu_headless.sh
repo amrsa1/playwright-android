86954b7a9e8a76079fc8c9703f3eef0e92352801
Amr Salem <32042806+amrsa1@users.noreply.github.com>
2023-03-06 00:36:56 +0100

    Update start_emu_headless.sh

--- start_emu_headless.sh
+++ start_emu_headless.sh
@@ -35,10 +35,10 @@ hw_accel_flag=$(check_hardware_acceleration)
 
 function launch_emulator () {
   adb devices | grep emulator | cut -f1 | xargs -I {} adb -s "{}" emu kill
-  options="@${emulator_name} -no-window -no-snapshot -noaudio -no-boot-anim ${hw_accel_flag} -camera-back none"
+  options="@${emulator_name} -no-window -no-snapshot-save -noaudio -no-boot-anim -memory 4096 ${hw_accel_flag} -camera-back none"
   if [[ "$OSTYPE" == *linux* ]]; then
     echo "${OSTYPE}: emulator ${options} -gpu off"
-    emulator $options -gpu off
+    emulator $options -gpu swiftshader_indirect
   fi
   if [[ "$OSTYPE" == *darwin* ]] || [[ "$OSTYPE" == *macos* ]]; then
     echo "${OSTYPE}: emulator ${options} -gpu swiftshader_indirect"
