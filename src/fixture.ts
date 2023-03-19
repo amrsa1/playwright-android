5c5cfca8f0c8db41e3b1bc194f0a9e3eda6db774
Amr Salem <amr.salem@pricehubble.com>
2023-03-20 00:45:55 +0100

    update

--- src/fixture.ts
+++ src/fixture.ts
@@ -1,6 +1,33 @@
 import { AndroidDevice, test as base, _android } from '@playwright/test';
 import LandingPage from '../src/pages/landingPage';
 
+const retries = 3;
+const timeoutMs = 12000;
+
+function timeoutPromise(ms: number | undefined) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject(new Error(`Timed out after ${ms}ms`));
+        }, ms);
+    });
+}
+
+async function instintiateConnection(func: { (): Promise<void>; (): any; }, retries: number, timeout: number | undefined) {
+    for (let i = 0; i < retries; i++) {
+        try {
+            const result = await Promise.race([func(), timeoutPromise(timeout)]);
+            return result;
+        } catch (error) {
+            console.error(`Error on attempt ${i + 1}: ${error}`);
+            if (i === retries - 1) {
+                throw error;
+            } else {
+                await new Promise(resolve => setTimeout(resolve, timeout));
+            }
+        }
+    }
+}
+
 let device: AndroidDevice;
 type MyFixtures = {
     landingPage: LandingPage;
@@ -9,37 +36,29 @@ type MyFixtures = {
 
 };
 
+
+
 const fixtures = base.extend<MyFixtures>({
 
     page: async ({ baseURL }, use) => {
-        try {
-            await _android.launchServer({ deviceSerialNumber: 'emulator-5554', omitDriverInstall: false })
-                .then(async res => {
-                    console.log(res.wsEndpoint())
-                    device = await _android.connect(res.wsEndpoint())
-                    await device.shell('pm clear com.android.chrome');
-                    await device.shell('am set-debug-app --persistent com.android.chrome')
-                    const context = await device.launchBrowser({
-                        baseURL: baseURL
+        instintiateConnection(
+            async () => {
+                await _android.launchServer({ deviceSerialNumber: 'emulator-5554', omitDriverInstall: false })
+                    .then(async (res) => {
+                        console.log(res.wsEndpoint());
+                        device = await _android.connect(res.wsEndpoint());
+                        await device.shell('pm clear com.android.chrome');
+                        await device.shell('am set-debug-app --persistent com.android.chrome');
+                        const context = await device.launchBrowser({
+                            baseURL: baseURL
+                        });
+                        const page = await context.newPage();
+                        await use(page);
                     });
-                    const page = await context.newPage();
-                    await use(page)
-                })
-        } catch (error) {
-            console.log(error)
-            await _android.launchServer({ deviceSerialNumber: 'emulator-5554', omitDriverInstall: false })
-                .then(async res => {
-                    console.log(res.wsEndpoint())
-                    device = await _android.connect(res.wsEndpoint())
-                    await device.shell('pm clear com.android.chrome');
-                    await device.shell('am set-debug-app --persistent com.android.chrome')
-                    const context = await device.launchBrowser({
-                        baseURL: baseURL
-                    });
-                    const page = await context.newPage();
-                    await use(page)
-                })
-        }
+            },
+            retries,
+            timeoutMs
+        );
 
     },
 
@@ -68,3 +87,4 @@ const fixtures = base.extend<MyFixtures>({
 });
 
 export { fixtures };
+
